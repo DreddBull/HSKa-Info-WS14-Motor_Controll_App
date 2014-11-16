688297bef8d42f8349b8f37c2a951754267cf5a3
Andreas Zacher <github@azacher.de>
2014-11-16 18:31:28 +0100

    Grobes Interface.

--- Interface.cs
+++ Interface.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Motor_Controll_App
 {
-    public partial class Form1 : Form
+    public partial class Interface : Form
     {
-        public Form1()
+        public Interface()
         {
             InitializeComponent();
         }
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Motor_Controll_App
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Interface());
         }
     }
 }
